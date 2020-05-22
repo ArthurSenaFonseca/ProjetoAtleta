4dcd6fefc81d5e951d609beb9e70bcdb9e8874b3
Arthur Sena Fonseca <rei.arthur.plua@hotmail.com>
2020-05-22 16:45:42 -0300

    Primeira alteração do form

---
+++ ProjetoAtleta/Atleta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAtleta
+{
+    class Atleta
+    {
+        private string nome;
+        private double altura;
+        private double peso;
+        private int idade;
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+        public double getAltura()
+        {
+            return altura;
+        }
+        public void getPeso(double peso)
+        {
+            this.peso = peso;
+        }
+        public double getPeso()
+        {
+            return peso;
+        }
+        public void getIdade(int idade)
+        {
+            this.idade = idade;
+        }
+        public double getIdade()
+        {
+            return idade;
+        }
+    }
+}
---
+++ ProjetoAtleta/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ProjetoAtleta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.lbl_idade = new System.Windows.Forms.Label();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(13, 13);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(84, 13);
+            this.lbl_nome.TabIndex = 0;
+            this.lbl_nome.Text = "Insira seu nome:";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(13, 30);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Insira sua altura:";
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(13, 83);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 3;
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(13, 120);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(85, 13);
+            this.lbl_peso.TabIndex = 4;
+            this.lbl_peso.Text = "Insera seu peso:";
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(13, 137);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 5;
+            // 
+            // lbl_idade
+            // 
+            this.lbl_idade.AutoSize = true;
+            this.lbl_idade.Location = new System.Drawing.Point(13, 173);
+            this.lbl_idade.Name = "lbl_idade";
+            this.lbl_idade.Size = new System.Drawing.Size(84, 13);
+            this.lbl_idade.TabIndex = 6;
+            this.lbl_idade.Text = "Insira sua idade:";
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(13, 190);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(100, 20);
+            this.txt_idade.TabIndex = 7;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(161, 25);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(133, 54);
+            this.btn_calcular.TabIndex = 8;
+            this.btn_calcular.Text = "Calcular IMC";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.BackColor = System.Drawing.Color.Red;
+            this.btn_sair.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_sair.Location = new System.Drawing.Point(161, 158);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(133, 50);
+            this.btn_sair.TabIndex = 9;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 220);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_idade);
+            this.Controls.Add(this.lbl_idade);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.lbl_nome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label lbl_idade;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_sair;
+    }
+}
+
